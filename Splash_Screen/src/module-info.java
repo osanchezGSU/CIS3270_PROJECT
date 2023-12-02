8d35373c423ee7a507002b3c06fb7502b002812a
osanchezGSU <150194031+osanchezGSU@users.noreply.github.com>
2023-12-02 16:01:51 -0500

    Deleted Splash_Screen Folder

--- Splash_Screen/src/module-info.java
+++
@@ -1,8 +0,0 @@
-module Splash_Screen {
-	requires javafx.controls;
-	requires javafx.graphics;
-	requires javafx.media;
-	requires javafx.fxml;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
