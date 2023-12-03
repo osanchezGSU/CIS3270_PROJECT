bec66ca7569eb59fc280d1efa9a36584e46d1215
osanchezGSU <150194031+osanchezGSU@users.noreply.github.com>
2023-12-03 13:55:42 -0500

    MainApplication Added

---
+++ CIS3270_FINAL_PROJECT/src/GUI/MainApplication.java
@@ -0,0 +1,73 @@
+package GUI;
+
+import javafx.animation.FadeTransition;
+import javafx.animation.PauseTransition;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class MainApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            // Load splash screen
+            FXMLLoader splashLoader = new FXMLLoader(getClass().getResource("Splash_Screen.fxml"));
+            Parent splashRoot = splashLoader.load();
+            Scene splashScene = new Scene(splashRoot);
+
+            // Set up audio in the SplashScreenController
+            Splash_Screen splashController = splashLoader.getController();
+            splashController.initialize();
+
+            // Create a pause before the fade-out animation
+            PauseTransition pause = new PauseTransition(Duration.seconds(2));
+            pause.setOnFinished(event -> {
+                // Create a fade transition for the splash screen
+                FadeTransition fadeOut = new FadeTransition(Duration.seconds(1), splashRoot);
+                fadeOut.setFromValue(1);
+                fadeOut.setToValue(0);
+                fadeOut.setOnFinished(fadeEvent -> {
+                    // Load the main application after the fade-out animation
+                    try {
+                        FXMLLoader mainLoader = new FXMLLoader(getClass().getResource("/GUI/Welcome.fxml"));
+                        Parent mainRoot = mainLoader.load();
+                        Scene mainScene = new Scene(mainRoot);
+
+                        // Set up the main stage
+                        primaryStage.setScene(mainScene);
+                        primaryStage.setTitle("Main Application");
+
+                        // Show the main scene with a fade-in animation
+                        FadeTransition fadeIn = new FadeTransition(Duration.seconds(1), mainRoot);
+                        fadeIn.setFromValue(0);
+                        fadeIn.setToValue(1);
+                        fadeIn.play();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+
+                // Play the fade-out animation
+                fadeOut.play();
+            });
+
+            // Show the splash screen
+            primaryStage.setScene(splashScene);
+            primaryStage.show();
+
+            // Start the delay before transitioning
+            pause.play();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
--- CIS3270_FINAL_PROJECT/src/GUI/Splash_Screen.java
+++ CIS3270_FINAL_PROJECT/src/GUI/Splash_Screen.java
@@ -1,42 +1,39 @@
 package GUI;
 
-import java.io.File;
-
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
+import javafx.application.Platform;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
-import javafx.stage.Stage;
 
-public class Splash_Screen extends Application {
-	
-	@Override
-	public void start(Stage primaryStage) throws Exception  {
-		
-		
-		
-		try {
-			
-			
-			Parent root = FXMLLoader.load(getClass().getResource("Splash_Screen.fxml"));
-			Scene scene = new Scene(root);
-			scene.getStylesheets().add(getClass().getResource("application.css").toString());
-	
-			primaryStage.setTitle("AeroBookings.com");
-			primaryStage.setFullScreen(false);
-			
-			primaryStage.setScene(scene);
-			primaryStage.show();
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+import java.io.File;
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+public class Splash_Screen {
+
+    private MediaPlayer mediaPlayer;
+
+    public void initialize() {
+        // Load and play audio
+        String audioFilePath = "/Users/osvaldosanchez/Downloads/airplane_sound.mp3";
+        Media media = new Media(new File(audioFilePath).toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.setAutoPlay(true);
+
+        // Simulate loading tasks (replace this with your actual loading logic)
+        new Thread(() -> {
+            try {
+                Thread.sleep(3000);
+
+                // Stop the audio when the loading is done
+                mediaPlayer.stop();
+
+                // Close the stage on the JavaFX Application Thread
+                Platform.runLater(() -> {
+                    // Assuming you have a reference to the stage, replace "yourStage" with the actual variable name
+                    // yourStage.close();
+                });
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
 }
