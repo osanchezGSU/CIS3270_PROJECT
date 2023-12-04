2203da7a52c280bd54d5e76a8137e0d5059a602b
celesjanette <celesaug1@gmail.com>
2023-12-03 22:14:20 -0500

    Database integration for all classes

--- CIS3270_FINAL_PROJECT/src/Database/BookingsDB.java
+++ CIS3270_FINAL_PROJECT/src/Database/BookingsDB.java
@@ -1,6 +1,28 @@
 package Database;
 
-public class BookingsDB {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class BookingsDB {
+	public static void main(String[] args) {
+    	try {
+    		Connection con = DriverManager.getConnection("jdbc:mysql:///AeroBookings","root","celes1213");
+    		Statement st = con.createStatement();
+    		String query = "select * from Bookings";
+    		ResultSet rs = st.executeQuery(query);
+    		while(rs.next()) {
+    			System.out.println(rs.getString(1)+ " "+ rs.getString(2));
+    		}
+    		con.close();
+    	}catch(SQLException e) {
+    		System.out.println("Error");
+    	} catch(Exception e) {
+    			
+
+    	}
+    }
 }
 
--- CIS3270_FINAL_PROJECT/src/Database/UsersDB.java
+++ CIS3270_FINAL_PROJECT/src/Database/UsersDB.java
@@ -1,6 +1,27 @@
 package Database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class UsersDB {
-   
+	public static void main(String[] args) {
+    	try {
+    		Connection con = DriverManager.getConnection("jdbc:mysql:///AeroBookings","root","celes1213");
+    		Statement st = con.createStatement();
+    		String query = "select * from Users";
+    		ResultSet rs = st.executeQuery(query);
+    		while(rs.next()) {
+    			System.out.println(rs.getString(1)+ " "+ rs.getString(2));
+    		}
+    		con.close();
+    	}catch(SQLException e) {
+    		System.out.println("Error");
+    	} catch(Exception e) {
+    			
+
+    	}
+    }
 }
