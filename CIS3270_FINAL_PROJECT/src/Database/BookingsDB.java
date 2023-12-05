2319e8a822617e134f668251f1cce3b30fd8e2ac
celesjanette <celesaug1@gmail.com>
2023-12-05 05:30:13 -0500

    BookingsDB update

--- CIS3270_FINAL_PROJECT/src/Database/BookingsDB.java
+++ CIS3270_FINAL_PROJECT/src/Database/BookingsDB.java
@@ -56,6 +56,7 @@ public class BookingsDB {
         } catch (ClassNotFoundException e) {
             throw new SQLException("MySQL JDBC Driver not found: " + e.getMessage());
         }
+        
         return connection;
     }
 }
