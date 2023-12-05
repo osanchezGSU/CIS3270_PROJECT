540ff88bd5eaf7b88947934492427ffcc5ecc78a
MantasKunigonis <mkunigonis1@student.gsu.edu>
2023-12-05 00:27:51 -0500

    Menu For User Home

---
+++ CIS3270_FINAL_PROJECT/src/GUI/Review_Orders.java
@@ -0,0 +1,5 @@
+package GUI;
+
+public class Review_Orders {
+
+}
--- CIS3270_FINAL_PROJECT/src/GUI/Scene_Controller.java
+++ CIS3270_FINAL_PROJECT/src/GUI/Scene_Controller.java
@@ -72,4 +72,13 @@ public class Scene_Controller {
 		
 	}
 	
+	public void switchToReviewOrders(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("Review_Orders.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+		
+	}
+	
 }
