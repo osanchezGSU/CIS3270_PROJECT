ecc080f5ceeded4c1e7e3a9ab91f18c8ff5675c0
celesjanette <celesaug1@gmail.com>
2023-12-05 16:59:44 -0500

    Updates to UserDBTEST and forgot password fxml

--- CIS3270_FINAL_PROJECT/src/Database/UserDBTEST.java
+++ CIS3270_FINAL_PROJECT/src/Database/UserDBTEST.java
@@ -2,28 +2,49 @@ package Database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserDBTEST {
-	
-	public Connection databaseLink; 
-	
-	public Connection getConnection() {
-		
-		String databaseName = "AeroBookings";
-		String databaseUser = "root";
-		String databasePassword = "root";
-		String url = "jdbc:mysql://localhost/" + databaseName;
-		
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			databaseLink = DriverManager.getConnection(url, databaseUser, databasePassword);
-		}catch (Exception e){
-			e.printStackTrace();
-		}
-		
-		return databaseLink;
-	}
+    
+    private Connection databaseLink;
+    
+    public Connection getConnection() {
+        String databaseName = "AeroBookings";
+        String databaseUser = "root";
+        String databasePassword = "root";
+        String url = "jdbc:mysql://localhost/" + databaseName;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            databaseLink = DriverManager.getConnection(url, databaseUser, databasePassword);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return databaseLink;
+    }
+
+    public void retrieveUserData(String query) {
+        try {
+            Connection conn = getConnection();
+            PreparedStatement statement = conn.prepareStatement(query);
+            ResultSet result = statement.executeQuery();
+            
+            // Process the ResultSet or perform necessary operations
+            while (result.next()) {
+                // Access columns and do something with the data
+                String columnData = result.getString("column_name");
+                System.out.println(columnData);
+            }
+
+            conn.close(); // Close the connection when done
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
 	
 	
