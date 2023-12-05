ebe834b70cf1c48fbc69217cb6176fdaba7ccf43
osanchezGSU <150194031+osanchezGSU@users.noreply.github.com>
2023-12-05 10:44:20 -0500

    User Register

--- CIS3270_FINAL_PROJECT/src/GUI/User_Home.java
+++ CIS3270_FINAL_PROJECT/src/GUI/User_Home.java
@@ -1,5 +1,261 @@
 package GUI;
 
-public class User_Home {
+import java.sql.Connection;
+import GUI.AdminMain;
+import GUI.CustomerMain;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import BusinessLogic.Booking;
+import BusinessLogic.Customer;
+import BusinessLogic.Flight;
+import Database.AdminDB;
+import Database.BookingDB;
+import Database.CustomerDB;
+import Database.FlightDB;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class User_Home extends Application implements EventHandler<ActionEvent> {
+	private static String bookFlightID = "";
+	static String dbSearch = "";
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		primaryStage.setTitle("Search Flights");
+		AnchorPane anchor = new AnchorPane();
+		anchor.setPadding(new Insets(20, 20, 20, 20));
+		TableView<Flight> table = new TableView<>();
+		final ObservableList<Flight> data = FXCollections.observableArrayList();
+
+		ChoiceBox<String> dropdown = new ChoiceBox<>();
+		dropdown.getItems().addAll("To", "From", "Date", "Time", "Airline");
+		dropdown.setValue("To");
+		dropdown.setLayoutY(60);
+		dropdown.setLayoutX(720);
+
+		Button userView = new Button("Main page");
+		userView.setOnAction(a -> {
+			if (AdminDB.isAdmin(homepage.getUsr())) {
+				primaryStage.close();
+				AdminMain main = new AdminMain();
+				main.start(new Stage());
+			} else {
+				primaryStage.close();
+				CustomerMain cmain = new CustomerMain();
+				cmain.start(new Stage());
+
+			}
+		});
+
+		TextField flightIDText = new TextField();
+		flightIDText.setPromptText("Flight ID");
+		flightIDText.setLayoutX(800);
+		flightIDText.setLayoutY(600);
+		flightIDText.setMinWidth(250);
+
+		Button bkFlight = new Button("Book Flight");
+		bkFlight.setLayoutX(1050);
+		bkFlight.setLayoutY(600);
+		bkFlight.setMinWidth(250);
+
+		TextField search = new TextField();
+		search.setLayoutX(800);
+		search.setLayoutY(60.0);
+		search.setMinWidth(250);
+
+		TableColumn<Flight, String> column1 = new TableColumn<Flight, String>("Flight#");
+		column1.setCellValueFactory(new PropertyValueFactory<>("flightNum"));
+		column1.setMinWidth(80);
+		TableColumn<Flight, String> column2 = new TableColumn<Flight, String>("Departure Date");
+		column2.setCellValueFactory(new PropertyValueFactory<>("departureDate"));
+		column2.setMinWidth(100);
+		TableColumn<Flight, String> column3 = new TableColumn<Flight, String>("Departure Time");
+		column3.setCellValueFactory(new PropertyValueFactory<>("departureTime"));
+		column3.setMinWidth(100);
+		TableColumn<Flight, String> column4 = new TableColumn<Flight, String>("Arrival Time");
+		column4.setCellValueFactory(new PropertyValueFactory<>("arrivalTime"));
+		column4.setMinWidth(100);
+		TableColumn<Flight, String> column5 = new TableColumn<Flight, String>("Flight Duration");
+		column5.setCellValueFactory(new PropertyValueFactory<>("flightDuration"));
+		column5.setMinWidth(100);
+		TableColumn<Flight, String> column6 = new TableColumn<Flight, String>("To");
+		column6.setCellValueFactory(new PropertyValueFactory<>("to"));
+		column6.setMinWidth(115);
+		TableColumn<Flight, String> column7 = new TableColumn<Flight, String>("From");
+		column7.setCellValueFactory(new PropertyValueFactory<>("from"));
+		column7.setMinWidth(115);
+		TableColumn<Flight, String> column8 = new TableColumn<Flight, String>("Airline");
+		column8.setCellValueFactory(new PropertyValueFactory<>("airlineName"));
+		column8.setMinWidth(80);
+		TableColumn<Flight, Integer> column9 = new TableColumn<Flight, Integer>("Capacity");
+		column9.setCellValueFactory(new PropertyValueFactory<>("capacity"));
+		column9.setMinWidth(75);
+		TableColumn<Flight, Integer> column10 = new TableColumn<Flight, Integer>("#Booked");
+		column10.setCellValueFactory(new PropertyValueFactory<>("numBooked"));
+		column10.setMinWidth(75);
+		TableColumn<Flight, String> column11 = new TableColumn<Flight, String>("Destination Airport");
+		column11.setCellValueFactory(new PropertyValueFactory<>("destinationAirport"));
+		column11.setMinWidth(128.88);
+		TableColumn<Flight, Double> column12 = new TableColumn<Flight, Double>("Flight Price");
+		column12.setCellValueFactory(new PropertyValueFactory<>("flight_price"));
+		column12.setMinWidth(80);
+		TableColumn<Flight, String> column13 = new TableColumn<Flight, String>("Boarding Time");
+		column13.setCellValueFactory(new PropertyValueFactory<>("boardingTime"));
+		column13.setMinWidth(110);
+		table.setTableMenuButtonVisible(false);
+		TableColumn<Flight, String> column14 = new TableColumn<Flight, String>("FlightID");
+		column14.setCellValueFactory(new PropertyValueFactory<>("flightID"));
+		column14.setMinWidth(80);
+
+		Button searchB = new Button("Search Flights");
+		searchB.setLayoutX(1050);
+		searchB.setLayoutY(60.0);
+		searchB.setMinWidth(100);
+
+		searchB.setOnAction(s -> {
+			try {
+			
+				SearchFlights.getChoice(dropdown);
+				String searchItem = search.getText().trim().toUpperCase();
+
+				if (dbSearch == "fDate" && !searchItem.matches("\\d{2}-\\d{2}-\\d{4}")) {
+					AlertMessage.display("Search Error", "Please insert date as MM-DD-YYYY and try again.");
+				}
+				if (dbSearch == "DepartureTime" && !searchItem.matches("\\d{2}:\\d{2}")) {
+					AlertMessage.display("Search Error",
+							"Please insert time as HH:MM (23:59 = 11:59 PM) and try again.");
+				}
+
+				
+					Connection connection = getConnection();
+
+					String str = "SELECT * FROM Flight WHERE " + dbSearch + " = '" + searchItem + "';";
+					PreparedStatement statement = connection.prepareStatement(str);
+
+					ResultSet myResult = statement.executeQuery();
+					table.getItems().clear();
+
+					if (!myResult.next()) {
+						AlertMessage.display("No Flights Found",
+								"No flights were found matching your criteria. Please try again. "
+										+ "\nFormat for searches-\nTo/From: City STATE ex. Atlanta GA\nDate:MM-DD-YYYY\nTime: HH:MM ex. 17:45");
+					}
+					while (myResult.next()) {
+						data.add(new Flight(myResult.getString("flightNum"), myResult.getString("fDate"),
+								myResult.getString("DepartureTime"), myResult.getString("ArrivalTime"),
+								myResult.getString("FlightDuration"), myResult.getString("fTo"),
+								myResult.getString("fFrom"), myResult.getString("AirlineName"),
+								myResult.getInt("capacity"), myResult.getInt("BookedNum"),
+								myResult.getString("DestinationAirport"), myResult.getString("Flight_Price"),
+								myResult.getString("BoardingTime"), myResult.getString(1)));
+						table.setItems(data);
+					}
+
+					statement.close();
+					myResult.close();
+					connection.close();
+
+				} catch (SQLException a) {
+					System.out.println(a.getMessage());
+				}
+			
+		});
+
+		table.setLayoutX(20);
+		table.setLayoutY(100);
+		table.setMinWidth(1020);
+		table.getColumns().addAll(column1, column2, column3, column4, column5, column6, column7, column8, column9,
+				column10, column11, column12, column13, column14);
+		anchor.getChildren().addAll(dropdown, search, searchB, table, userView, bkFlight, flightIDText);
+		Scene scene = new Scene(anchor, 1600, 900);
+
+		bkFlight.setOnAction(f -> {
+			setBookFlightID(flightIDText.getText());
+			if (BookingDB.checkConflict(getBookFlightID(), homepage.getUsr()) && BookingDB.checkCapacity()) {
+				AlertMessage.display("Flight Conflict",
+						"WARNING!!! The FlightID entered creates a conflict with a previously booked flight!");
+				primaryStage.close();
+				BookFlight main2 = new BookFlight();
+				main2.start(new Stage());
+			} else if (!BookingDB.checkConflict(getBookFlightID(), homepage.getUsr()) && !BookingDB.checkDoubleBooked()
+					&& !BookingDB.checkCapacity()) {
+				primaryStage.close();
+				BookFlight main2 = new BookFlight();
+				main2.start(new Stage());
+
+			} else if (BookingDB.checkConflict(getBookFlightID(), homepage.getUsr()) && BookingDB.checkDoubleBooked()) {
+				AlertMessage.display("Flight Conflict",
+						"The FlightID entered creates a conflict with a previously booked flight & has already been booked.");
+			} else if (BookingDB.checkDoubleBooked()) {
+				AlertMessage.display("Flight Conflict", "The FlightID entered has already been booked.");
+			} else if (BookingDB.checkCapacity()) {
+				AlertMessage.display("Flight Conflict",
+						"Sorry!! The FlightID entered is over capacity. Please choose another flight.");
+			}
+
+		});
+
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	public void handle(ActionEvent arg0) {
+
+	}
+
+	// test
+	public static void getChoice(ChoiceBox<String> dropdown) {
+		String choice = ((String)dropdown.getValue());
+		if (choice.equals("To")) {
+			dbSearch = "fTo";
+		} else if (choice.equals("From")) {
+			dbSearch = "fFrom";
+		} else if (choice.equals("Date")) {
+			dbSearch = "fDate";
+		} else if (choice.equals("Time")) {
+			dbSearch = "DepartureTime";
+		} else if (choice.equals("Airline")) {
+			dbSearch = "AirlineName";
+		}
+		
+	}
+
+	public static Connection getConnection() {
+		Connection connection = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost/WrightFlights?useSSL=false", "root",
+					"root");
+		} catch (Exception e) {
+			System.out.println("Cannot connect");
+		}
+		return connection;
+
+	}
+
+	public static String getBookFlightID() {
+		return bookFlightID;
+	}
+
+	public static void setBookFlightID(String bookFlightID) {
+		SearchFlights.bookFlightID = bookFlightID;
+	}
 }
--- CIS3270_FINAL_PROJECT/src/GUI/User_Registration.java
+++ CIS3270_FINAL_PROJECT/src/GUI/User_Registration.java
@@ -1,6 +1,172 @@
 package GUI;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import Database.UserDBTEST;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.scene.text.Text;
+
+
 public class User_Registration {
+	 private Stage stage;
+	 private Scene scene;
+	 private Parent root;
+	
+	 @FXML
+	 private PasswordField setPassword;
+	 
+	 @FXML
+	 private PasswordField confirmPassword;
+	 
+	 @FXML
+	 private PasswordField setSSN;
+	 
+	 @FXML
+	 private TextField setUsername;
+	 
+	 @FXML
+	 private TextField setFirstName;
+	 
+	 @FXML
+	 private TextField setLastName;
+	 
+	 @FXML
+	 private TextField setEmail;
+	 
+	 @FXML
+	 private TextField setStreetAddress;
+	 
+	 @FXML
+	 private TextField setZipCode;
+	 
+	 @FXML
+	 private TextField setState;
 
-}
+	 @FXML
+	 private Text errorMessage;
+	 
 
+	 @FXML
+	 private Button submitButton;
+	 
+	  public void switchToWelcome(ActionEvent event) throws IOException {
+	        Parent root = FXMLLoader.load(getClass().getResource("Welcome.fxml"));
+	        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+	        scene = new Scene(root);
+	        stage.setScene(scene);
+	        stage.show();
+	    }
+	  
+	  public void switchToUserHome() throws IOException {
+	        Parent root = FXMLLoader.load(getClass().getResource("User_Home.fxml"));
+	        stage = (Stage) setFirstName.getScene().getWindow();
+	        scene = new Scene(root);
+	        stage.setScene(scene);
+	        stage.show();
+	    }
+	  
+	  public void registerButtonAction(Event event) {
+		  if (setPassword.getText().equals(confirmPassword.getText())) {
+			  registerUser();
+			  Platform.runLater(() -> {
+                  try {
+                      switchToUserHome();
+                  } catch (IOException e) {
+                      e.printStackTrace();
+                  }
+              });
+          } else {
+			  errorMessage.setText("Password does not match");
+			  
+		  }
+	  }
+
+	  public void registerUser() {
+	        UserDBTEST connectNow = new UserDBTEST();
+	        Connection connectDB = connectNow.getConnection();
+
+	        String firstName = setFirstName.getText();
+	        String lastName = setLastName.getText();
+	        String username = setUsername.getText();
+	        String password = setPassword.getText();
+	        String email = setEmail.getText();
+	        String ssn = setSSN.getText();
+	        String address = setStreetAddress.getText();
+	        String zipcode = setZipCode.getText();
+	        String state = setState.getText();
+
+	        // Check for blank fields
+	        if (firstName.isEmpty() || lastName.isEmpty() || username.isEmpty() || password.isEmpty() || email.isEmpty() || ssn.isEmpty() || address.isEmpty() || zipcode.isEmpty() || state.isEmpty()) {
+	            errorMessage.setText("Please fill in all fields.");
+	            return;
+	        }
+
+	        // Check if username is already taken
+	        if (isUsernameTaken(username)) {
+	            errorMessage.setText("Username is already taken. Please choose a different one.");
+	            return;
+	        }
+
+	        String insertToRegister = "INSERT INTO Users(First_Name, Last_Name, Address, Zip_Code, State, Username, Password, Email, SSN) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+	        try {
+	            PreparedStatement preparedStatement = connectDB.prepareStatement(insertToRegister);
+	            preparedStatement.setString(1, firstName);
+	            preparedStatement.setString(2, lastName);
+	            preparedStatement.setString(3, address);
+	            preparedStatement.setString(4, zipcode);
+	            preparedStatement.setString(5, state);
+	            preparedStatement.setString(6, username);
+	            preparedStatement.setString(7, password);
+	            preparedStatement.setString(8, email);
+	            preparedStatement.setString(9, ssn);
+
+	            preparedStatement.executeUpdate();
+
+	            Platform.runLater(() -> {
+	                try {
+	                    switchToUserHome();
+	                } catch (IOException e) {
+	                    e.printStackTrace();
+	                }
+	            });
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	            // Handle the exception, e.g., display an error message
+	        }
+	    }
+
+	    private boolean isUsernameTaken(String username) {
+	        UserDBTEST connectNow = new UserDBTEST();
+	        Connection connectDB = connectNow.getConnection();
+	        String query = "SELECT * FROM Users WHERE Username = ?";
+
+	        try (PreparedStatement preparedStatement = connectDB.prepareStatement(query)) {
+	            preparedStatement.setString(1, username);
+	            ResultSet resultSet = preparedStatement.executeQuery();
+
+	            return resultSet.next(); // If there is a result, the username is taken
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	            // Handle the exception, e.g., display an error message
+	        }
+
+	        return false; // Default to not taken in case of an error
+	    }
+	}
