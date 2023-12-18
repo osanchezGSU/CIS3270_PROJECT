6e36200c69cee3a636ad3963abf6010bbe04aae7
celesjanette <celesaug1@gmail.com>
2023-12-18 13:18:02 -0500

    Major Database changes to all files.
    All files are needed in order to run application

--- CIS3270_FINAL_PROJECT/src/Database/UserDBTEST.java
+++ CIS3270_FINAL_PROJECT/src/Database/UserDBTEST.java
@@ -2,7 +2,7 @@ package Database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
+
 
 public class UserDBTEST {
 	
--- CIS3270_FINAL_PROJECT/src/Database/UsersDB.java
+++ CIS3270_FINAL_PROJECT/src/Database/UsersDB.java
@@ -5,43 +5,48 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Timestamp;
 
-public class BookingsDB {
-    // Database connection details
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/AeroBookings"; 
+public class UsersDB {
+
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/AeroBookings";
     private static final String DB_USER = "root";
     private static final String DB_PASSWORD = "root";
 
     public static void main(String[] args) {
-
-        retrieveBookingData();
+        retrieveUserData();
     }
 
-    public static void retrieveBookingData() {
+    public static void retrieveUserData() {
         try (Connection connection = getConnection()) {
-         
-            String query = "SELECT booking_id, flight_id, booking_time, user_id FROM Bookings";
+
+            String query = "SELECT First_Name, Last_Name, Username, Password, Email, SSN, Address, State, Zip_Code FROM Users";
 
             PreparedStatement preparedStatement = connection.prepareStatement(query);
             ResultSet resultSet = preparedStatement.executeQuery();
-           
-            while (resultSet.next()) {
-            
-                int bookingId = resultSet.getInt("booking_id");
-                String flightId = resultSet.getString("flight_id");
-                Timestamp bookingTime = resultSet.getTimestamp("booking_time");
-                int userId = resultSet.getInt("user_id");
 
-             
-                System.out.println("Booking ID: " + bookingId +
-                        ", Flight ID: " + flightId +
-                        ", Booking Time: " + bookingTime.toString() +
-                        ", User ID: " + userId);
+            while (resultSet.next()) {
+                String firstName = resultSet.getString("First_Name");
+                String lastName = resultSet.getString("Last_Name");
+                String username = resultSet.getString("Username");
+                String password = resultSet.getString("Password");
+                String email = resultSet.getString("Email");
+                String ssn = resultSet.getString("SSN");
+                String address = resultSet.getString("Address");
+                String state = resultSet.getString("State");
+                String zipCode = resultSet.getString("Zip_Code");
+
+                System.out.println("First Name: " + firstName +
+                        ", Last Name: " + lastName +
+                        ", Username: " + username +
+                        ", Password: " + password +
+                        ", Email: " + email +
+                        ", SSN: " + ssn +
+                        ", Address: " + address +
+                        ", State: " + state +
+                        ", Zip Code: " + zipCode);
             }
         } catch (SQLException e) {
             e.printStackTrace();
-        
         }
     }
 
@@ -50,13 +55,14 @@ public class BookingsDB {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-            System.out.println("Connected to the AeroBookings database!"); 
-        } catch (ClassNotFoundException e) {
-            throw new SQLException("MySQL JDBC Driver not found: " + e.getMessage());
+            System.out.println("Connected to the AeroBookings database!");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            throw new SQLException("Failed to connect to the database.");
         }
-        
-     return connection;
+        return connection;
     }
 }
+
 
     
